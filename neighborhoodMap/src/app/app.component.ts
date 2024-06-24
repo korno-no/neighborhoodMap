6b53ca99ee972b67027b82abc7ff1183a5334648
Korno <korno9612@gmail.com>
2024-06-24 23:55:47 +0300

    add main map

--- neighborhoodMap/src/app/app.component.ts
+++ neighborhoodMap/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Loader } from '@googlemaps/js-api-loader';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'neighborhoodMap';
+
+  ngOnInit(): void {
+   
+    let loader = new Loader({
+      apiKey: '***'
+    })
+    loader.load().then (()=>{
+      const map = new google.maps.Map(document.getElementById('map')!,{
+        center: {
+          lat: 57.995498561052,
+          lng: 56.15747333711451
+        },
+        zoom: 17
+      })
+    })
+
+    
+  }
 }
