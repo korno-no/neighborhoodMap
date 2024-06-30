b9d28adbf19f83859c5ec95d850f9cc98abec13c
Korno <korno9612@gmail.com>
2024-06-30 20:51:17 +0300

    add marker functionality

--- neighborhoodMap/src/app/app.component.ts
+++ neighborhoodMap/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Loader } from '@googlemaps/js-api-loader';
+import { GoogleMap } from '@angular/google-maps';
 
 @Component({
   selector: 'app-root',
@@ -10,20 +11,34 @@ export class AppComponent implements OnInit{
   title = 'neighborhoodMap';
 
   ngOnInit(): void {
-   
+    let map: google.maps.Map | null = null;
     let loader = new Loader({
-      apiKey: '***'
-    })
-    loader.load().then (()=>{
-      const map = new google.maps.Map(document.getElementById('map')!,{
-        center: {
-          lat: 57.995498561052,
-          lng: 56.15747333711451
-        },
-        zoom: 17
-      })
-    })
+      apiKey: '',
+      version: 'weekly'
+    });
+    const center = {
+      lat: 57.995498561052,
+      lng: 56.15747333711451
+    };
 
-    
+    loader.load().then(() => {
+      // map object with coordinates
+      map = new google.maps.Map(document.getElementById('map')!, {
+        center: center,
+        zoom: 17
+      });
+    }).catch(e => {
+      console.error("Error loading Google Maps: ", e);
+    });
+
+    async function addMarker() {
+      const { AdvancedMarkerElement } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
+      const marker = new AdvancedMarkerElement({
+        map: map,
+        position: center,
+        title: 'Uluru'
+      });
+    }
+    addMarker();
   }
 }
